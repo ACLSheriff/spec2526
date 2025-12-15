a334ee4bfd228c8297ab78d5d47499d274cd5191
ACLSheriff <ava.sheriff@icloud.com>
2025-12-15 12:23:34 +0000

    starting development on a new project done some coding and added a database

---
+++ northpole_inc/v1/assests/common.php
@@ -0,0 +1,51 @@
+<?php
+
+function new_user($conn, $post)//creates fuction
+{
+
+        $sql = "INSERT INTO users (firstname, surname, username,password, d_o_b, address) VALUES(?,?,?,?,?,?)";//easy to sql attack
+        $stmt = $conn->prepare($sql);//prepare sql
+
+    $stmt->bindValue(1, $post['firstname']);//binds values
+    $stmt->bindValue(2, $post['surname']);
+    $stmt->bindValue(3, $post['username']);
+    $hpswd = password_hash($post['password'], PASSWORD_DEFAULT);//built in libray to incrypt
+    $stmt->bindValue(4, $hpswd);
+    $stmt->bindValue(5, $post['d_o_b']);
+    $stmt->bindValue(6, $post['address']);// binding the data from form to SQL statment this makes it more secure from a SQL injection attack less likly to hijk
+
+    $stmt->execute();// run the query to insert
+        $conn = null;// gets rid of connection to make sure no open connection which is secrity breach
+        return true;
+}
+
+function user_message()
+{
+    if(isset($_SESSION['usermessage'])){//check if message set
+        $message = "<p>". $_SESSION['usermessage']."</p>";//assinges the massage and styles
+        unset($_SESSION['usermessage']);//unsets message
+        return $message;//return message
+    }else{//if not met
+        $message = "";//set to blank
+        return $message;//returns message
+    }
+
+}
+
+function username_check($conn, $username)
+{
+
+        $sql = "SELECT username FROM users where username= ?";//sql stament getting usernames from database
+        $stmt = $conn->prepare($sql);//prepare sql
+        $stmt->bindValue(1, $username);//subbmits paramitter so its secure
+        $stmt->execute();//run sql code
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);//brings back results
+        $conn = null;// stops the connection so more secure
+        if ($result) {//checks if got anything back
+            return true;
+        } else {
+            return false;
+        }
+
+
+}
