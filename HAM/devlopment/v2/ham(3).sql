38c0303efbe5fc45036a06d4807d8d40130c9fc6
ACLSheriff <ava.sheriff@icloud.com>
2026-01-19 08:54:05 +0000

    starting development on a new project done some coding and added a database

--- HAM/devlopment/v2/ham(3).sql
+++ HAM/devlopment/v2/ham(3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 09, 2026 at 02:25 PM
+-- Generation Time: Jan 19, 2026 at 08:53 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.26
 
@@ -33,7 +33,7 @@ CREATE TABLE `assets` (
   `longdesc` text NOT NULL,
   `details` text NOT NULL,
   `room_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -44,8 +44,25 @@ CREATE TABLE `assets` (
 CREATE TABLE `house` (
   `house_id` int NOT NULL,
   `reg_date` date NOT NULL,
-  `address` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `address` text NOT NULL,
+  `street` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `house`
+--
+
+INSERT INTO `house` (`house_id`, `reg_date`, `address`, `street`) VALUES
+(1, '2026-01-16', 'hx2 8ya', '5 hello street'),
+(2, '2026-01-16', 'hx2 8ya', '4 hello street'),
+(3, '2026-01-16', 'hx2 8ya', '7 hello street'),
+(4, '2026-01-16', 'hx2 8ya', '9 hello street'),
+(5, '2026-01-16', 'hx2 8ya', '11 hello street'),
+(6, '2026-01-16', 'hx2 8ya', '1 hope street'),
+(7, '2026-01-16', 'hx2 8ya', '1 hope street'),
+(8, '2026-01-16', 'hx2 8ya', '45'),
+(9, '2026-01-16', 'hx2 8ya', '45'),
+(10, '2026-01-16', 'hx2 8ya', '45');
 
 -- --------------------------------------------------------
 
@@ -59,7 +76,7 @@ CREATE TABLE `rooms` (
   `floor` text NOT NULL,
   `house_id` int NOT NULL,
   `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -74,7 +91,14 @@ CREATE TABLE `users` (
   `address` text NOT NULL,
   `firstname` text NOT NULL,
   `surname` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `address`, `firstname`, `surname`) VALUES
+(1, 'avas', '$2y$10$k.Xo9z2nhiUjzrIEzReqCO5CnSUZbLISEt0Xk0v792XGFMd3RZCmm', 'ghgh', 'ava', 'sheriff');
 
 -- --------------------------------------------------------
 
@@ -83,11 +107,19 @@ CREATE TABLE `users` (
 --
 
 CREATE TABLE `veiws` (
+  `views_id` int NOT NULL,
   `house_id` int NOT NULL,
   `longdesc` text NOT NULL,
   `role` text NOT NULL,
   `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `veiws`
+--
+
+INSERT INTO `veiws` (`views_id`, `house_id`, `longdesc`, `role`, `user_id`) VALUES
+(7, 10, 'owner of the house', 'owner', 1);
 
 --
 -- Indexes for dumped tables
@@ -124,6 +156,7 @@ ALTER TABLE `users`
 -- Indexes for table `veiws`
 --
 ALTER TABLE `veiws`
+  ADD PRIMARY KEY (`views_id`),
   ADD KEY `user_id` (`house_id`),
   ADD KEY `user_id_2` (`user_id`);
 
@@ -141,7 +174,7 @@ ALTER TABLE `assets`
 -- AUTO_INCREMENT for table `house`
 --
 ALTER TABLE `house`
-  MODIFY `house_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `house_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `rooms`
@@ -153,7 +186,13 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `veiws`
+--
+ALTER TABLE `veiws`
+  MODIFY `views_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -169,15 +208,14 @@ ALTER TABLE `assets`
 -- Constraints for table `rooms`
 --
 ALTER TABLE `rooms`
-  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `veiws`
 --
 ALTER TABLE `veiws`
-  ADD CONSTRAINT `veiws_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `veiws_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `veiws_ibfk_2` FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `veiws_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
